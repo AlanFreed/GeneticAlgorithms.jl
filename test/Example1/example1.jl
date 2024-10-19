d3e9a40881974209cb88c3c4f6c5aaa891cc5152
Alan D. Freed <alan.d.freed@protonmail.com>
2024-10-19 05:21:20 -0400

    version 1.0.0

--- test/Example1/example1.jl
+++ test/Example1/example1.jl
@@ -2,201 +2,232 @@ module example1
 
 # To use this code, you will need to download the genetic algorithm at:
 # using Pkg
+# Pkg.add(url = "https://github.com/AlanFreed/PhysicalFields.jl")
 # Pkg.add(url = "https://github.com/AlanFreed/GeneticAlgorithms.jl")
 
 using
-    CairoMakie,       # Pixel based figure construction.
+    CairoMakie,
     GeneticAlgorithms,
-    Statistics
+    PhysicalFields
+
+import
+    GeneticAlgorithms as GA,
+    PhysicalFields    as PF
 
 export
-    Ex1Species,
-    runModel,
     run
 #=
 --------------------------------------------------------------------------------
 =#
 
-struct Ex1Species <: AbstractSpecies
-    nExp::Int64                    # number of experiments
-    nCtl::Vector{Int64}            # number of control  variables per experiment
-    nRes::Vector{Int64}            # number of response variables per experiment
-    nPts::Vector{Int64}            # number of datum points per experiment
-    ctrl::Vector{Matrix{Float64}}  # control  data for  each experiment
-    resp::Vector{Matrix{Float64}}  # response data from each experiment
-    stdR::Vector{Vector{Float64}}  # standard deviation in the responses per exp
-
-    # constructor
-
-    function Ex1Species()
-
-        nExp = 1
-        nCtl = Vector{Int64}(undef, nExp)
-        nCtl[1] = 1     # i.e., strain
-        nRes = Vector{Int64}(undef, nExp)
-        nRes[1] = 1     # i.e., stress
-        nPts = Vector{Int64}(undef, nExp)
-        nPts[1] = 20
-        ctrl = Vector{Matrix{Float64}}(undef, nExp)
-        mtxC = Matrix{Float64}(undef, nCtl[1], nPts[1])
-        mtxC[1,1]  = 0.1
-        mtxC[1,2]  = 0.2
-        mtxC[1,3]  = 0.3
-        mtxC[1,4]  = 0.4
-        mtxC[1,5]  = 0.5
-        mtxC[1,6]  = 0.6
-        mtxC[1,7]  = 0.7
-        mtxC[1,8]  = 0.8
-        mtxC[1,9]  = 0.9
-        mtxC[1,10] = 1.0
-        mtxC[1,11] = 1.1
-        mtxC[1,12] = 1.2
-        mtxC[1,13] = 1.3
-        mtxC[1,14] = 1.4
-        mtxC[1,15] = 1.5
-        mtxC[1,16] = 1.6
-        mtxC[1,17] = 1.7
-        mtxC[1,18] = 1.8
-        mtxC[1,19] = 1.9
-        mtxC[1,20] = 2.0
-        ctrl[1] = mtxC
-        resp = Vector{Matrix{Float64}}(undef, nExp)
-        mtxR = Matrix{Float64}(undef, nRes[1], nPts[1])
-        mtxR[1,1]  = 0.050
-        mtxR[1,2]  = 0.111
-        mtxR[1,3]  = 0.193
-        mtxR[1,4]  = 0.290
-        mtxR[1,5]  = 0.349
-        mtxR[1,6]  = 0.450
-        mtxR[1,7]  = 0.559
-        mtxR[1,8]  = 0.622
-        mtxR[1,9]  = 0.744
-        mtxR[1,10] = 0.835
-        mtxR[1,11] = 1.032
-        mtxR[1,12] = 1.144
-        mtxR[1,13] = 1.266
-        mtxR[1,14] = 1.396
-        mtxR[1,15] = 1.409
-        mtxR[1,16] = 1.494
-        mtxR[1,17] = 1.625
-        mtxR[1,18] = 1.675
-        mtxR[1,19] = 1.700
-        mtxR[1,20] = 1.710
-        resp[1] = mtxR
-        stdR = Vector{Vector{Float64}}(undef, nExp)
-        stdV = Vector{Float64}(undef, nRes[1])
-        stdV[1] = std(mtxR[1,:])
-        stdR[1] = stdV
-        new(nExp, nCtl, nRes, nPts, ctrl, resp, stdR)
+function myData()::GA.TheData
+    n_exp = 1
+    pairs = 1
+    n_pts = 20
+    
+    experiments     = n_exp
+    conjugate_pairs = Vector{Int}(undef, n_exp)
+    data_points     = Vector{Int}(undef, n_exp)
+    for exp in 1:n_exp
+        conjugate_pairs[exp] = pairs
+        data_points[exp]     = n_pts
     end
-end # Ex1Species
+    
+    time     = Vector{PF.ArrayOfPhysicalScalars}(undef, 0) # a static model
+    control  = Vector{Vector{PF.ArrayOfPhysicalScalars}}(undef, n_exp)
+    response = Vector{Vector{PF.ArrayOfPhysicalScalars}}(undef, n_exp)
+    for exp in 1:n_exp
+        control[exp]  = Vector{PF.ArrayOfPhysicalScalars}(undef, pairs)
+        response[exp] = Vector{PF.ArrayOfPhysicalScalars}(undef, pairs)
+        for pair in 1:pairs
+            control[exp][pair]  = PF.ArrayOfPhysicalScalars(n_pts, PF.STRAIN)
+            response[exp][pair] = PF.ArrayOfPhysicalScalars(n_pts, PF.STRESS)
+        end
+    end
+    control[1][1][1]  = PF.PhysicalScalar(0.1, PF.STRAIN)
+    control[1][1][2]  = PF.PhysicalScalar(0.2, PF.STRAIN)
+    control[1][1][3]  = PF.PhysicalScalar(0.3, PF.STRAIN)
+    control[1][1][4]  = PF.PhysicalScalar(0.4, PF.STRAIN)
+    control[1][1][5]  = PF.PhysicalScalar(0.5, PF.STRAIN)
+    control[1][1][6]  = PF.PhysicalScalar(0.6, PF.STRAIN)
+    control[1][1][7]  = PF.PhysicalScalar(0.7, PF.STRAIN)
+    control[1][1][8]  = PF.PhysicalScalar(0.8, PF.STRAIN)
+    control[1][1][9]  = PF.PhysicalScalar(0.9, PF.STRAIN)
+    control[1][1][10] = PF.PhysicalScalar(1.0, PF.STRAIN)
+    control[1][1][11] = PF.PhysicalScalar(1.1, PF.STRAIN)
+    control[1][1][12] = PF.PhysicalScalar(1.2, PF.STRAIN)
+    control[1][1][13] = PF.PhysicalScalar(1.3, PF.STRAIN)
+    control[1][1][14] = PF.PhysicalScalar(1.4, PF.STRAIN)
+    control[1][1][15] = PF.PhysicalScalar(1.5, PF.STRAIN)
+    control[1][1][16] = PF.PhysicalScalar(1.6, PF.STRAIN)
+    control[1][1][17] = PF.PhysicalScalar(1.7, PF.STRAIN)
+    control[1][1][18] = PF.PhysicalScalar(1.8, PF.STRAIN)
+    control[1][1][19] = PF.PhysicalScalar(1.9, PF.STRAIN)
+    control[1][1][20] = PF.PhysicalScalar(2.0, PF.STRAIN)
+    
+    response[1][1][1]  = PF.PhysicalScalar(0.050, PF.STRESS)
+    response[1][1][2]  = PF.PhysicalScalar(0.111, PF.STRESS)
+    response[1][1][3]  = PF.PhysicalScalar(0.193, PF.STRESS)
+    response[1][1][4]  = PF.PhysicalScalar(0.290, PF.STRESS)
+    response[1][1][5]  = PF.PhysicalScalar(0.349, PF.STRESS)
+    response[1][1][6]  = PF.PhysicalScalar(0.450, PF.STRESS)
+    response[1][1][7]  = PF.PhysicalScalar(0.559, PF.STRESS)
+    response[1][1][8]  = PF.PhysicalScalar(0.622, PF.STRESS)
+    response[1][1][9]  = PF.PhysicalScalar(0.744, PF.STRESS)
+    response[1][1][10] = PF.PhysicalScalar(0.835, PF.STRESS)
+    response[1][1][11] = PF.PhysicalScalar(1.032, PF.STRESS)
+    response[1][1][12] = PF.PhysicalScalar(1.144, PF.STRESS)
+    response[1][1][13] = PF.PhysicalScalar(1.266, PF.STRESS)
+    response[1][1][14] = PF.PhysicalScalar(1.396, PF.STRESS)
+    response[1][1][15] = PF.PhysicalScalar(1.409, PF.STRESS)
+    response[1][1][16] = PF.PhysicalScalar(1.494, PF.STRESS)
+    response[1][1][17] = PF.PhysicalScalar(1.625, PF.STRESS)
+    response[1][1][18] = PF.PhysicalScalar(1.675, PF.STRESS)
+    response[1][1][19] = PF.PhysicalScalar(1.700, PF.STRESS)
+    response[1][1][20] = PF.PhysicalScalar(1.710, PF.STRESS)
+    
+    return GA.TheData(experiments, conjugate_pairs, data_points, 
+                      time, control, response)
+end # myData
 
-function _ex1Model(ϵ::Float64, θ::Vector{Float64})::Float64
-    # NOTE: the model's parameters are to index from 2,
-    # as θ[1] is an internal parameter assigned by the optimizer.
-    a = θ[2]
-    b = θ[3]
-    E = θ[4]
-    σ = E * sinh(a*ϵ) / (a * cosh(a*ϵ) - b * sinh(a*ϵ))
+mutable struct MyParameters <: GA.AbstractParameters
+    a::PF.PhysicalScalar
+    b::PF.PhysicalScalar
+    E::PF.PhysicalScalar
+    
+    function MyParameters()
+        a = PF.PhysicalScalar(PF.DIMENSIONLESS)
+        b = PF.PhysicalScalar(PF.DIMENSIONLESS)
+        E = PF.PhysicalScalar(PF.MODULUS)
+        new(a, b, E)::MyParameters
+    end
+end # MyParameters
+
+myparameters = MyParameters()
+mydata  = myData()
+mymodel = GA.Model{MyParameters}(myparameters::MyParameters,
+                                 mydata::GA.TheData)
+
+function _mysolve(mymodel::GA.Model;
+                  exp::Int, pair::Int, datum::Int)::PF.PhysicalScalar
+    # Example of a static model described by a simple function, i.e., σ = f(ε).
+    ε = mymodel.data.control[exp][pair][datum]
+    θ = mymodel.θ
+    σ = θ.E * sinh(θ.a*ε) / (θ.a * cosh(θ.a*ε) - θ.b * sinh(θ.a*ε))
     return σ
-end # _ex1Model
+end # _mysolve
 
-function runModel(s::Ex1Species, θ::Vector{Float64})::Vector{Matrix{Float64}}
-    # Retrieve the data held within MySpecies s.
-    nExp = experiments(s)
-    nRes = responsesPerExp(s)
-    nPts = dataPointsPerExp(s)
-    ctrl = controls(s)
-    # This function returns the model's response as an array of dimension:
-    #   [nExp] x [nRes x nPts]  indexed as  [i][j,k]
-    #       nExp    number of experiments the model is to be fit against
-    #       nRes    number of  responses  for each experiment  i
-    #       nPts    number of data points for each experiment  i
-    # Create the returned array that is to hold the model's response.
-    modR = Vector{Matrix{Float64}}(undef, nExp)
-    for exp in 1:nExp
-        mtxR = Matrix{Float64}(undef, nRes[exp], nPts[exp])
-        modR[exp] = mtxR
-    end
-    # Run your model subject to Ex1Species controls, viz., subject to ctrl.
-    for i in 1:nExp
-        for j in 1:nRes[i]
-            for k in 1:nPts[i]
-                ϵ = ctrl[i][j,k]
-                σ = _ex1Model(ϵ, θ)
-                modR[i][j,k] = σ
+function GA.solve!(::MyParameters, mymodel::GA.Model)
+    # Type MyParameters allows Julia to select solve! via multiple dispatch.
+    for exp in 1:mymodel.data.experiments
+        for pair in 1:mymodel.data.conjugate_pairs[exp]
+            for datum in 1:mymodel.data.data_points[exp]
+                y = _mysolve(mymodel; exp, pair, datum)
+                mymodel.data.response_mod[exp][pair][datum] = y
             end
         end
     end
-    return modR
-end # runModel
+    return nothing
+end # GA.solve!
 
-function run()
-    species = Ex1Species
-    probabilityOfMutation  = 0.01
-    probabilityOfCrossover = 0.95
-    probabilityOfImmigrant = 0.005
-    parameterNames = Vector{String}(undef, 4)
-    parameterNames[1] = "p in p-norm, i.e., ‖x‖ₚ = (∑ₙ₌₁ᴺ|xₙ|^p)^(1/p),  p ≥ 1"
-    parameterNames[2] = "coefficient a"
-    parameterNames[3] = "coefficient b"
-    parameterNames[4] = "effective Young's modlus E (dynes/cm²)"
-    alienParameters = Vector{Float64}(undef, 0)
-    minParameters = Vector{Float64}(undef, 4)
-    maxParameters = Vector{Float64}(undef, 4)
-    minParameters[1] = 1.0
-    minParameters[2] = 0.1
-    minParameters[3] = 0.1
-    minParameters[4] = 0.1
-    maxParameters[1] = 100.0
-    maxParameters[2] = 1.5
-    maxParameters[3] = 1.5
-    maxParameters[4] = 1.5
-    significantFigures = 4
-
-    c = Colony(species, probabilityOfMutation, probabilityOfCrossover, probabilityOfImmigrant, parameterNames, alienParameters, minParameters, maxParameters, significantFigures)
-
-    ga = GeneticAlgorithm(c)
-    run(ga)
-
-    # Create a figure of the best fit for the model.
-    ϵₑ = zeros(Float64, 20)
-    σₑ = zeros(Float64, 20)
-    for i in 1:20
-        ϵₑ[i] = ga.c.species.ctrl[1][1,i]
-        σₑ[i] = ga.c.species.resp[1][1,i]
+function plot(colony::Colony)
+    # Create a figure illustrating a best fit of the model.
+    εₑ = Vector{Float64}
+    σₑ = Vector{Float64}
+    σₘ = Vector{Float64}
+    for exp in 1:colony.mydata.experiments
+        εₑ = zeros(Float64, colony.mydata.data_points[exp])
+        σₑ = zeros(Float64, colony.mydata.data_points[exp])
+        σₘ = zeros(Float64, colony.mydata.data_points[exp])
+        for pair in 1:colony.mydata.conjugate_pairs[exp]
+            for datum in 1:colony.mydata.data_points[exp]
+                εₑ[datum] = PF.get(colony.mydata.control[exp][pair][datum])
+                σₑ[datum] = PF.get(colony.mydata.response_exp[exp][pair][datum])
+                σₘ[datum] = PF.get(colony.mydata.response_mod[exp][pair][datum])
+            end
+        end
     end
-    N  = 150
-    dϵ = 2.0 / (N - 1)
-
-    θ  = parameters(ga.c.elite)
-    ϵₘ = zeros(Float64, N)
-    σₘ = zeros(Float64, N)
-    for i in 2:N
-        ϵₘ[i] = (i - 1) * dϵ
-        σₘ[i] = _ex1Model(ϵ[i], θ)
-    end
-
-    fig = Figure(; size = (809, 500)) # (500ϕ, 500), ϕ is golden ratio
-    ax = Axis(fig[1, 1];
-        xlabel = "strain ϵ",
-        ylabel = "stress σ (dynes/cm²)",
-        title = "Genetic Algorithm Fit of Data",
+    
+    σᵢ    = zeros(Float64, 2)
+    σᵢ[2] = PF.get(colony.mydata.response_exp[1][1][colony.mydata.data_points[1]])
+    
+    fig = Figure(; size = (1000, 500))
+    ax1 = Axis(fig[1, 1];
+        xlabel = "strain ε",
+        ylabel = "stress σ (Pa)",
+        title = "Genetic Algorithm",
         titlesize = 24,
+        xlabelsize = 20,
         ylabelsize = 20)
-    scatter!(ax, ϵₑ, σₑ;
+    scatter!(ax1, εₑ, σₑ;
         marker = :circle,
         markersize = 10,
         color = :black,
         label = "data")
-    lines!(ax, ϵₘ, σₘ;
+    lines!(ax1, εₑ, σₘ;
         linewidth = 3,
         linestyle = :solid,
         color = :black,
         label = "model")
     axislegend("Legend",
         position = :lt)
-    save(string(pwd(), "/GA_fit_2_data.png"), fig)
+    ax2 = Axis(fig[1, 2];
+        xlabel = "experimental stress σₑ (Pa)",
+        ylabel = "model for stress σₘ (Pa)",
+        title = "Fit to Data",
+        titlesize = 24,
+        xlabelsize = 20,
+        ylabelsize = 20)
+    scatter!(ax2, σₑ, σₘ;
+        marker = :circle,
+        markersize = 10,
+        color = :black,
+        label = "σₑ vs. σₘ")
+    lines!(ax2, σᵢ, σᵢ;
+        linewidth = 1,
+        linestyle = :solid,
+        color = :black,
+        label = "perfect fit")
+    axislegend("Legend",
+        position = :lt)
+    save(string(pwd(), "/files/GA_example1.png"), fig)
+end # plot
+
+function run()
+    # Specify it the report is to be verbose or not.
+    verbose = false
+
+    myparameters           = MyParameters()
+    mydata                 = myData()
+    probability_mutation   = 0.01 
+    probability_crossover  = 0.95
+    probability_immigrant  = 0.005
+    parameters_alien       = Vector{PF.PhysicalScalar}(undef, 0)    # no alien
+    parameters_min         = Vector{PF.PhysicalScalar}(undef, 3)
+    parameters_max         = Vector{PF.PhysicalScalar}(undef, 3)
+    parameters_constrained = Vector{Tuple{Int,Int}}(undef, 1) 
+    significant_figures    = 6
+
+    # Populate parameter bounds and constraints.
+    parameters_min[1] = PF.PhysicalScalar(0.1, PF.DIMENSIONLESS)
+    parameters_min[2] = PF.PhysicalScalar(0.1, PF.DIMENSIONLESS)
+    parameters_min[3] = PF.PhysicalScalar(0.1, PF.MODULUS)
+
+    parameters_max[1] = PF.PhysicalScalar(1.5, PF.DIMENSIONLESS)
+    parameters_max[2] = PF.PhysicalScalar(1.5, PF.DIMENSIONLESS)
+    parameters_max[3] = PF.PhysicalScalar(1.5, PF.MODULUS)
+
+    parameters_constrained[1] = (2, 1)  # b < a
+    
+    # Create the colony and its genetic algorithm.
+    colony = GA.Colony(myparameters, mydata, probability_mutation,
+                       probability_crossover, probability_immigrant,
+                       parameters_alien, parameters_min, parameters_max,
+                       parameters_constrained, significant_figures)
+
+    genetic_algorithm = GA.GeneticAlgorithm(colony)
+    GA.run!(genetic_algorithm; verbose)
+    plot(genetic_algorithm.colony)
 end # run
 
 end # example1
